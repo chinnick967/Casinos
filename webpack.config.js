ad22798b3d379b0ed02c600246af1d0b09baafad
leagueofcards <admin@lol.cards>
2018-01-08 23:01:51 -0800

    webpack opt

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 module.exports = {
   // context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
+  devtool: 'source-map',
   entry: path.resolve(__dirname, "./src/index.js"),
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -24,5 +24,10 @@ module.exports = {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
   ],
 };
